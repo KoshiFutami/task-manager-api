b2fa4dc08de217bb133ef839e596a593e90d4c52
KoshiFutami <pizza.lover.kf@gmail.com>
2026-05-03 21:01:55 +0900

    refactor: Module に TaskRepositoryImpl を DI 登録
    - ITaskRepository をトークンとして使用
    - TaskRepositoryImpl をプロバイダーとして登録
    - NestJS の DI コンテナが正しく解決できるように設定

--- src/tasks/tasks.module.ts
+++ src/tasks/tasks.module.ts
@@ -3,10 +3,18 @@ import { TasksController } from './presentation/controllers/tasks.controller';
 import { TasksService } from './application/services/tasks.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Task } from './infrastructure/persistence/task.entity';
+import { TaskRepositoryImpl } from './infrastructure/task.repository';
+import { ITaskRepository } from './domain/repositories/task.repository';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Task])],
   controllers: [TasksController],
-  providers: [TasksService],
+  providers: [
+    TasksService,
+    {
+      provide: ITaskRepository,
+      useClass: TaskRepositoryImpl,
+    },
+  ],
 })
 export class TasksModule {}
